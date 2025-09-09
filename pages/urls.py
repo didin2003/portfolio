e8ee0b7751c190c4ecd1a6da10cc095cdd2c67bb
Didin PG <didingangadharpg@gmail.com>
2025-09-09 17:36:22 +0530

    Portfolio

---
+++ pages/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("contact/", views.contact_submit, name="contact_submit"),
+    path("online_shop/", views.online_shop, name="online_shop"),
+    path("health_stack/", views.health_stack, name="health_stack"),
+    path("classmanager/", views.classmanager, name="classmanager"),
+    path("lapzone/", views.lapzone, name="lapzone"),
+    path("gym/", views.gym, name="gym"),
+    path("train_food/", views.train_food, name="train_food"),
+    path("contact_view/", views.contact_view, name="contact_view"),
+]
