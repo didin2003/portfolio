80941e5170ea3f9cbc9aacaf2da1017ae5c883df
Didin <didinpg8080@gmail.com>
2025-09-17 12:26:38 +0000

    Portfolio

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "dev-secret-change-me"
 DEBUG = False
-ALLOWED_HOSTS = ["sonu.didin.in"]
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
